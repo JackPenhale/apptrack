dc02bc903d7477cee32f9e8f2ebd595f55d3654f
JackPenhale <jackpenhale@gmail.com>
2023-07-06 21:19:16 -0600

    get rid of authform

--- src/app/components/Authform.tsx
+++ src/app/components/Authform.tsx
@@ -1,64 +1,64 @@
-'use client'
+// 'use client'
 
-import { type } from "os";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+// import { type } from "os";
+// import { useCallback, useState } from "react";
+// import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
-type variant = 'LOGIN' | 'REGISTER';
+// type variant = 'LOGIN' | 'REGISTER';
 
-const AuthFrom = () => {
-    const [variant, setVariant] = useState<variant>('LOGIN');
-    const [isLoading, setIsLoading] = useState(false);
+// const AuthFrom = () => {
+//     const [variant, setVariant] = useState<variant>('LOGIN');
+//     const [isLoading, setIsLoading] = useState(false);
 
-const toggleVariant = useCallback(() => {
-    if(variant === 'LOGIN') {
-        setVariant('REGISTER');
-    } else {
-        setVariant('LOGIN');
-    }
-}, [variant]);
+// const toggleVariant = useCallback(() => {
+//     if(variant === 'LOGIN') {
+//         setVariant('REGISTER');
+//     } else {
+//         setVariant('LOGIN');
+//     }
+// }, [variant]);
 
-const {
-    register,
-    handleSubmit,
-    formState: {
-        errors
-    }
-} = useForm<FieldValues>({
-    defaultValues: {
-        name: '',
-        email: '',
-        password: ''.
-    }
-});
+// const {
+//     register,
+//     handleSubmit,
+//     formState: {
+//         errors
+//     }
+// } = useForm<FieldValues>({
+//     defaultValues: {
+//         name: '',
+//         email: '',
+//         password: ''.
+//     }
+// });
 
-const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
+// const onSubmit: SubmitHandler<FieldValues> = (data) => {
+//     setIsLoading(true);
 
-    if(variant === 'REGISTER') {
-        //axios register
-    } 
-    if(variant === 'LOGIN') {
-        //nextauth login
-    }
-}
+//     if(variant === 'REGISTER') {
+//         //axios register
+//     } 
+//     if(variant === 'LOGIN') {
+//         //nextauth login
+//     }
+// }
 
-const socialAction = (action:string) => {
-    setIsLoading(true);
-}
+// const socialAction = (action:string) => {
+//     setIsLoading(true);
+// }
 
-return(
-    <div
-    className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6 " onSubmit={handleSubmit(onSubmit)}></form>
+// return(
+//     <div
+//     className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+//         <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+//             <form className="space-y-6 " onSubmit={handleSubmit(onSubmit)}></form>
             
-        </div>
+//         </div>
 
-    </div>
-)
+//     </div>
+// )
 
 
-}
+// }
 
-export default AuthFrom
+// export default AuthFrom
